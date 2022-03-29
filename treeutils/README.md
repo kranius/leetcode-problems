ffcd55a461a0ddd0113a2e63bfeec01729092b39
Guillaume Daniel <g.daniel.buca@gmail.com>
2022-03-29 12:34:32 +0200

    added Readme and 94-inorder traversal

---
+++ treeutils/README.md
@@ -0,0 +1,10 @@
+# Tree problems from leetcode
+
+These are my solutions for leetcode tree problems.
+
+I'm a bit rusty (starting with the easy ones), probably not the most optimized code.
+
+
+##### Implemented so far :
+
+- 94. Binary Tree Inorder Traversal
--- treeutils/src/main/java/com/kranius/treeutils/TreeUtils.java
+++ treeutils/src/main/java/com/kranius/treeutils/TreeUtils.java
@@ -4,12 +4,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 // implementations of leetcode tree manipulation functions
-// for convenience I choose to implement them as static methods
 public class TreeUtils<T> {
 	
+	// 94. Binary Tree Inorder Traversal
 	// inorder traversal = left first
 	public List<T> inorderTraversal(TreeNode<T> root) {
-		return new ArrayList<T>();
+		List<T> result = new ArrayList<T>();
+		
+		inorderTraversalHelper(root, result);
+		
+		return result;
+	}
+	
+	//  helper for 94. inorderTraversal
+	private void inorderTraversalHelper(TreeNode<T> root, List<T> result) {
+		if (root == null)
+			return;
+		
+		if (root.left != null)
+			inorderTraversalHelper(root.left, result);
+		
+		result.add(root.val); // we add value after all left nodes are traversed
+		
+		if (root.right != null)
+			inorderTraversalHelper(root.right, result);
 	}
 
 }
