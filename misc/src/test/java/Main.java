f08d7accb25b43e9bbd63562e9ada9c321d3f31c
Guillaume Daniel <g.daniel.buca@gmail.com>
2022-05-01 19:27:10 +0200

    added 168 Intersection of two Linked List

--- misc/src/test/java/Main.java
+++ misc/src/test/java/Main.java
@@ -2,11 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Misc misc = new Misc();
 
-        String test1 = "A man, a plan, a canal: Panama";
-        String test2 = "0P";
-        String test3 = "race a car";
-        String test4 = "a.b,.";
-
-        System.out.println(misc.cleanString(test4));
+        System.out.println(misc.convertToTitle(1));
+        System.out.println(misc.convertToTitle(701));
     }
 }
